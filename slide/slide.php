950f93ea2611650638bdbf35b5a5a797d04a5482
YanglyYangly <yangly932@gmail.com>
2022-08-10 10:43:15 +0700

    create slide

---
+++ slide/slide.php
@@ -0,0 +1,34 @@
+<?php  
+   header("Access-Control-Allow-Origin: *");
+   header("Content-Type: application/json; charset=UTF-8");
+   
+   $response = array();  
+  
+      require_once $_SERVER['DOCUMENT_ROOT']. '/api/config/Database.php';   
+      // connecting to db  
+      $db = new Database();  
+      $query = "SELECT img_slide from tbl_slides where is_show =1;";
+      
+
+      $result = mysqli_query($db->connect(),$query);  
+      
+      if (mysqli_num_rows($result) > 0) {  
+ 
+         $response["status"] = array("code"=>200,"message"=>"success");
+         $response["data"] = array();  
+         $response["data"]['slides'] = array();
+
+         array_push($response["data"]['slides'], mysqli_fetch_assoc($result));  
+           
+         echo json_encode($response);  
+
+      } else {  
+
+         $response["status"] = array("code"=>400,"message"=>"Missing error");
+
+         echo json_encode($response);  
+
+      }  
+
+   
+?> 
