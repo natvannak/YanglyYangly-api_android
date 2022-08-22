5627061d2cb2c91a419ace58937b336fa84a0e2e
YanglyYangly <yangly932@gmail.com>
2022-08-22 10:07:02 +0700

    Add dashboardparent

--- dashboardparent/dashboardparent.php
+++ dashboardparent/dashboardparent.php
@@ -2,18 +2,16 @@
    header("Access-Control-Allow-Origin: *");
    header("Content-Type: application/json; charset=UTF-8");
    $response = array();
-   if (isset($_POST['parents_id'])) {  
-      $parents_id = $_POST['parents_id']; 
-
-   require_once $_SERVER['DOCUMENT_ROOT']. '/api_android/config/Database.php';    
+   if (isset($_GET['id'])) { 
+   require_once $_SERVER['DOCUMENT_ROOT']. '/api_android/config/Database.php';
       // connecting to db  
    $db = new Database();
 
-   $query = "SELECT tbl_students.img_path, tbl_students.name_kh, tbl_groups.group_name_kh FROM tbl_student_parents
-    INNER JOIN tbl_students ON tbl_students.id=tbl_student_parents.stu_id
-    INNER JOIN tbl_groups ON tbl_groups.id=tbl_student_parents.stu_id 
-      WHERE tbl_student_parents.stu_id= 1 and tbl_students.status = 1 AND tbl_student_parents.parents_id =1";
-
+   $query = "SELECT tbl_student_parents.stu_id, tbl_students.img_path, tbl_students.name_kh, tbl_groups.group_name_kh  FROM tbl_student_parents
+   INNER JOIN tbl_students ON tbl_students.id=tbl_student_parents.stu_id
+   INNER JOIN tbl_study_records ON tbl_study_records.id=tbl_student_parents.stu_id 
+    INNER JOIN tbl_groups ON tbl_groups.id=tbl_student_parents.stu_id
+   WHERE tbl_student_parents.parents_id = ".$_GET['id'];
 
    $result = mysqli_query($db->connect(),$query);  
    
