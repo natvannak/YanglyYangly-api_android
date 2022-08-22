47905b6f49239236c5997a7798c0badcf94ca226
YanglyYangly <yangly932@gmail.com>
2022-08-22 16:34:25 +0700

    add contact

--- contact_info/contact_info.php
+++ contact_info/contact_info.php
@@ -2,15 +2,15 @@
    header("Access-Control-Allow-Origin: *");
    header("Content-Type: application/json; charset=UTF-8");
    $response = array();
-   if (isset($_POST['academic_year']) && isset($_POST['stu_id'])) {  
-      $academic = $_POST['academic_year']; 
-      $stu_id = $_POST['stu_id']; 
-
+   if (isset($_GET['id'])) {   
+       
    require_once $_SERVER['DOCUMENT_ROOT']. '/api_android/config/Database.php';    
       // connecting to db  
    $db = new Database();
 
-   $query = "SELECT * FROM tbl_contact_info";
+   $query = "SELECT tbl_staffs.id, tbl_staffs.name_kh, tbl_staffs.phone, tbl_staffs.telegram, tbl_staff_job_pos.position FROM tbl_staffs
+   INNER JOIN tbl_staff_job_pos ON tbl_staff_job_pos.id=tbl_staffs.staff_id
+   INNER JOIN tbl_students ON tbl_students.id=tbl_staffs.staff_id";
 
    $result = mysqli_query($db->connect(),$query);  
    
@@ -26,7 +26,7 @@
       echo json_encode($response);  
 
    } else {  
-
+      
       $response["status"] = array("code"=>204,"message"=>"No Data");
 
       echo json_encode($response);  
