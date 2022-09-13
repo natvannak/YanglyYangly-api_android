c848b160cadea4ca5c60d9b14aeee29696027c96
nat-vannak <natvannak007@gmail.com>
2022-09-13 13:47:11 +0700

    Modify Schedule OK

--- schedule/schedule.php
+++ schedule/schedule.php
@@ -3,27 +3,23 @@
    header("Content-Type: application/json; charset=UTF-8");
    
    $response = array();  
-   if (isset($_POST['academic_year']) && isset($_POST['month']) && isset($_POST['stu_id'])) {  
-      $academic = $_POST['academic_year']; 
-      $month = $_POST['month']; 
-      $stu_id = $_POST['stu_id']; 
-
+   if (isset($_GET['academic_year']) && isset($_GET['day']) && isset($_GET['stu_id'])) {  
+      $academic = $_GET['academic_year']; 
+      $day = $_GET['day']; 
+      $stu_id = $_GET['stu_id']; 
 
       require_once $_SERVER['DOCUMENT_ROOT']. '/api/config/Database.php';   
-      // connecting to db  
+     
       $db = new Database();  
-      // $query = "SELECT study_hour FROM tbl_schedule_details 
-      /*$query = "SELECT tbl_schedule_details.id, tbl_study_hours.hour_start, tbl_subjects.subject_kh,tbl_teachers.name_kh  
-      FROM tbl_schedule_details 
-      INNER JOIN tbl_subjects ON tbl_subjects.id=tbl_schedule_details.subject 
-      INNER JOIN tbl_teachers ON tbl_teachers.id=tbl_schedule_details.teacher
-      INNER JOIN tbl_study_hours ON tbl_study_hours.id=tbl_schedule_details.study_hour"; 
-*/
-      $query = "SELECT *  
-      FROM tbl_schedule_details 
-      INNER JOIN tbl_subjects ON tbl_subjects.id=tbl_schedule_details.subject 
-      INNER JOIN tbl_teachers ON tbl_teachers.id=tbl_schedule_details.teacher
-      INNER JOIN tbl_study_hours ON tbl_study_hours.id=tbl_schedule_details.study_hour"; 
+
+      $query = "SELECT vw_schedule_details.study_hour_all, vw_schedule_details.day_kh, vw_schedule_details.subject_kh, vw_schedule_details.teacher_kh 
+      FROM vw_schedules 
+      INNER JOIN vw_schedule_details ON vw_schedule_details.schedule_id = vw_schedules.id
+      INNER JOIN tbl_study_records ON tbl_study_records.`group` = vw_schedules.`group`
+      WHERE tbl_study_records.id_student = ". $stu_id .
+      " AND tbl_study_records.school_year = ". $academic .
+      " AND vw_schedule_details.weekday = ". $day .
+      " ORDER BY study_hour_all ASC ;";
       
 
       $result = mysqli_query($db->connect(),$query);  
@@ -32,21 +28,20 @@
  
          $response["status"] = array("code"=>200,"message"=>"success");
          $response["data"] = array();  
-         $response["data"]['Schedule'] = array();
-
-         
+         $response["data"]['schedule'] = array();
+  
           while ($row = mysqli_fetch_assoc($result)) {
-             array_push($response["data"]['Schedule'], $row);  
+             array_push($response["data"]['schedule'], $row);  
           } 
-         echo json_encode($response);  
 
       } else {  
 
          $response["status"] = array("code"=>400,"message"=>"Missing error");
-
-         echo json_encode($response);  
-
       }  
 
    }  
+   else{
+      $response["status"] = array("code"=>400,"message"=>"Empty ID");
+   } 
+   echo json_encode($response); 
 ?> 
